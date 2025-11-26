eb648551be695c419071db750674b1833629329a
impri2 <impri@naver.com>
2025-11-26 21:44:29 +0900

    implement checkpoint loading

--- src/frequencies/get_frequencies.py
+++ src/frequencies/get_frequencies.py
@@ -1,6 +1,7 @@
 import csv
 import glob
 import json
+import os
 import requests
 
 import tqdm
@@ -19,45 +20,105 @@ def get_word_frequency(word:str,corpus='v4_olmo-2-0325-32b-instruct_llama')->int
         except:
             pass
     return result['count']
-def get_wordlist_frequencies(input_path,output_path):
-    frequencies = []
-    with open(input_path) as f:
-        reader = csv.DictReader(f)
-        for row in tqdm.tqdm(reader):
-            word = row['Word']
-            level = row['Level']
-            frequency = get_word_frequency(word)
-            frequencies.append({'word':word,'level':level,'frequency':frequency})
-    with open(output_path,'w'):
-        json.dump(frequencies,f)
-def make_frequency_dataset(langs = ['en', 'ja', 'ko', 'zh']):
-    
+def make_frequency_dataset(start_cnt = 0):
+    langs = ['en', 'ja', 'ko', 'zh']
+    output_path = f'data/frequencies/frequency_data.json'
+    frequencies = {'en':[],'ja':[],'ko':[],'zh':[]} 
+    if start_cnt != 0:
+        with open(output_path[:-5] + str(start_cnt) + '.json') as f:
+            frequencies = json.load(f)
+    start_word_found = False
+    row_cnt = 0
     for lang in langs:
         print(f"making {lang} dataset")
-        wordlist_path = f'data/wordlist_{lang}.csv'
-        output_path = f'data/frequencies/frequency_data_{lang}.json'
-        get_wordlist_frequencies(wordlist_path,output_path)
-        print(f"finished making {lang} dataset")
-
-def get_unknown_frequencies(langs = ['en', 'ja', 'ko', 'zh']):
+        wordlist_path = f'data/wordlist_{lang}.csv' 
+        with open(wordlist_path) as f:
+            reader = csv.DictReader(f)
+            for row in tqdm.tqdm(reader):
+                row_cnt += 1
+                word = row['Word']
+                level = row['Level']
+                print(f"processing word {row_cnt}: {word}")
+                if row_cnt >= start_cnt:
+                    start_word_found = True
+                if not start_word_found:
+                    continue
+                frequency = get_word_frequency(word)
+                frequencies[lang].append({'word':word,'level':level,'frequency':frequency})
+                if row_cnt % 100 == 0:
+                    with open(output_path[:-5] + str(row_cnt) + '.json','w') as f:
+                        json.dump(frequencies,f)
+                    print(f"Saved: {row_cnt}")
+    with open(output_path,'w') as f:
+        json.dump(frequencies,f)
+        
+    print(f"finished making dataset")
+def get_unknown_tokens(start_cnt=0):
     unknown_token_finder = UnknownTokenFinder()
     langs = ['en', 'ja', 'ko', 'zh']
+    unknown_tokens = {'en':[],'ja':[],'ko':[],'zh':[]}
+    output_path = 'data/frequencies/unknown_tokens.json'
+    if start_cnt != 0:
+        with open(output_path[:-5] + str(start_cnt) + '.json') as f:
+            unknown_tokens = json.load(f)
+    file_cnt = 0
+    start_file_found = False
     for lang in langs:
-        unknown_tokens = set()
-        unknown_token_frequencies = {}
         files = glob.glob(f'data/wikipedia/parsed_wikitext/{lang}/*.json')
         files.sort()
-        print(f'{lang} has {len(files)} files')
         for file in tqdm.tqdm(files):
+            file_cnt += 1
+            print(f'analyzing file #{file_cnt}: {file}...')
+            if file_cnt >= start_cnt:
+                start_file_found = True
+            if not start_file_found:
+                continue
             with open(file) as f:
-                unknown_tokens = unknown_token_finder.find_unknown_tokens([json.load(f)['plain_text']],[lang])
-            for token in unknown_tokens:
-                unknown_token_frequencies[token] = get_word_frequency(token)
-            with open(f'data/frequencies/{lang}/unknown_token_frequencies_{lang}_{file}.json') as f:
-                json.dump(f,unknown_token_frequencies)
+                file_unknown_tokens = unknown_token_finder.find_unknown_tokens([json.load(f)['plain_text']],[lang])
+            unknown_tokens[lang].extend(file_unknown_tokens)
+            if file_cnt % 100 == 0:
+                with open(output_path[:-5] + str(file_cnt) + '.json','w') as f:
+                    json.dump(unknown_tokens,f)
+                print(f"Saved: {file_cnt}")
+
+    with open(output_path,'w') as f:
+        json.dump(unknown_token_frequencies,f)
+    print(f"finished  get unknown tokens")
+
+def get_unknown_token_frequencies(start_cnt=0):
+    langs = ['en', 'ja', 'ko', 'zh']
+    unknown_token_frequencies = {'en':dict(),'ja':dict(),'ko':dict(),'zh':dict()}
+    output_path = 'data/frequencies/unknown_tokens_frequencies.json'
+    with open('data/frequencies/unknown_tokens.json') as f:
+        unknown_tokens = json.load(f)
+    if start_cnt != 0:
+        with open(output_path[:-5] + str(start_cnt) + '.json') as f:
+            unknown_token_frequencies = json.load(f)
+    token_cnt = 0
+    for lang in langs:
+        tokens = list(set(unknown_tokens[lang]))
+        tokens.sort()
+        for token in tokens:
+            token_cnt += 1
+            print(f"processing token {token_cnt}")
+            if token_cnt < start_cnt:continue
+            unknown_token_frequencies[lang][token] = get_word_frequency(token)
+            if token_cnt % 100 == 0:
+                with open(output_path[:-5] + str(token_cnt) + '.json','w') as f:
+                    json.dump(unknown_token_frequencies,f)
+                    print(f"Saved: {token_cnt}")
+    with open(output_path,'w') as f:
+        json.dump(f,unknown_token_frequencies)
+    print(f"finished get unknown token frequencies")
 
 
 if __name__ == '__main__':
-    make_frequency_dataset(langs = ['en', 'ja', 'ko', 'zh'])
-    get_unknown_frequencies(langs = ['en', 'ja', 'ko', 'zh'])
+    os.makedirs('data/frequencies',exist_ok=True)
+    make_frequency_dataset()
+    get_unknown_tokens()
+    get_unknown_token_frequencies()
+    '''make_frequency_dataset(300)
+    get_unknown_frequencies(100)
+    get_unknown_token_frequencies(1000)'''
+
     
