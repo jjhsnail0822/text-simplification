278bbe530b05f36a6f86280b179536feec6cc91c
impri2 <impri@naver.com>
2025-12-03 21:37:20 +0900

    fix get unknown tokens to use chunked dataset

--- src/frequencies/get_frequencies.py
+++ src/frequencies/get_frequencies.py
@@ -8,6 +8,8 @@ import tqdm
 from unknown_token_finder import UnknownTokenFinder
 
 SAVE_INTERVAL = 100
+from datasets import load_from_disk
+
 
 def get_word_frequency(word:str,corpus='v4_olmo-2-0325-32b-instruct_llama')->int:
 
@@ -60,67 +62,67 @@ def make_frequency_dataset(start_cnt = 0):
         json.dump(frequencies,f)
         
     print(f"finished making dataset")
-def get_unknown_tokens(start_cnt=0):
+def get_unknown_tokens():
+    dataset = load_from_disk('data/wikipedia/dataset/all')
+    docs = {'en':[],'ja':[],'ko':[],'zh':[]}
+    for data in dataset['train']:
+        docs[data['language']].append(data['plain_text'])
     unknown_token_finder = UnknownTokenFinder()
     langs = ['en', 'ja', 'ko', 'zh']
     unknown_tokens = {'en':[],'ja':[],'ko':[],'zh':[]}
     output_path = 'data/frequencies/unknown_tokens.json'
-    if start_cnt != 0:
+    try:
         with open(output_path[:-5] + '_intermediate' + '.json') as f:
             unknown_tokens = json.load(f)
-    file_cnt = 0
-    start_file_found = False
+    except:
+        pass
     for lang in langs:
-        files = glob.glob(f'data/wikipedia/parsed_wikitext/{lang}/*.json')
-        files.sort()
-        for file in tqdm.tqdm(files):
-            file_cnt += 1
-            if file_cnt >= start_cnt:
-                start_file_found = True
-            if not start_file_found:
-                continue
-            with open(file) as f:
-                file_unknown_tokens = unknown_token_finder.find_unknown_tokens([json.load(f)['plain_text']],[lang])
-            unknown_tokens[lang].extend(file_unknown_tokens)
-            if file_cnt % SAVE_INTERVAL == 0:
+        print(f"get unknown {lang} tokens")
+        start_pos = len(unknown_tokens[lang])
+        for i,document in enumerate(tqdm.tqdm((docs[lang][start_pos:]))):
+            document_unknown_tokens = unknown_token_finder.find_unknown_tokens([document],[lang])
+            unknown_tokens[lang].append(document_unknown_tokens)
+            if len(unknown_tokens[lang]) % SAVE_INTERVAL == 0:
                 update_checkpoint(output_path,unknown_tokens)
 
     with open(output_path,'w') as f:
         json.dump(unknown_token_frequencies,f)
     print(f"finished  get unknown tokens")
 
-def get_unknown_token_frequencies(start_cnt=0):
+def get_unknown_token_frequencies():
     langs = ['en', 'ja', 'ko', 'zh']
     unknown_token_frequencies = {'en':dict(),'ja':dict(),'ko':dict(),'zh':dict()}
     output_path = 'data/frequencies/unknown_tokens_frequencies.json'
     with open('data/frequencies/unknown_tokens.json') as f:
         unknown_tokens = json.load(f)
-    if start_cnt != 0:
+    try:
         with open(output_path[:-5] + '_intermediate' + '.json') as f:
             unknown_token_frequencies = json.load(f)
-    token_cnt = 0
+    except:
+        pass
     for lang in langs:
-        tokens = list(set(unknown_tokens[lang]))
+        print(f"get {lang} unknown token frequencies")
+        start_pos = len(unknown_token_frequencies[lang])
+        tokens = []
+        for token_list in unknown_tokens[lang]:
+            tokens.extend(token_list)
+        tokens = list(set(tokens))
         tokens.sort()
-        for token in tqdm.tqdm(tokens):
-            token_cnt += 1
-            if token_cnt < start_cnt:continue
+        print('start pos:',start_pos)
+        for i,token in enumerate(tqdm.tqdm(tokens[start_pos:])):
             unknown_token_frequencies[lang][token] = get_word_frequency(token)
-            if token_cnt % SAVE_INTERVAL == 0:
+            if len(unknown_token_frequencies[lang]) % SAVE_INTERVAL == 0:
                 update_checkpoint(output_path,unknown_token_frequencies)
 
     with open(output_path,'w') as f:
-        json.dump(f,unknown_token_frequencies)
+        json.dump(unknown_token_frequencies,f)
     print(f"finished get unknown token frequencies")
 
 
 if __name__ == '__main__':
     os.makedirs('data/frequencies',exist_ok=True)
-    make_frequency_dataset()
-    get_unknown_tokens()
+    #make_frequency_dataset() # already done
+    get_unknown_tokens() 
     get_unknown_token_frequencies()
-    '''make_frequency_dataset(300)
-    get_unknown_frequencies(100)
-    get_unknown_token_frequencies(1000)'''
 
     
